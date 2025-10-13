096d74faf6289c4722df75fc8554f063ba58efe8
IamInaph <inough123@gmail.com>
2025-10-13 06:42:31 +0545

    Updated Header, Footer, and Article page

--- React_Base_Layouts/424/src/components/Footer/Footer.jsx
+++ React_Base_Layouts/424/src/components/Footer/Footer.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer component</div>;
+  return (
+    <footer className="bg-teal-900 text-teal-200 text-sm text-center py-6 mt-16 transition-colors hover:text-coral-400">
+      © 2025 Website_Name. All rights reserved.
+    </footer>
+  );
 }
